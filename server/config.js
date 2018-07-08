c0b2db62116f362128661f511c8c53c9c9aa7979
Prateek Madapurmath <prateekmadapurmath@Prateeks-MacBook-Air.local>
2018-07-07 21:00:35 -0700

    update config

--- server/config.js
+++ server/config.js
@@ -1,13 +1,16 @@
 const env = process.env.NODE_ENV || "prod";
 
 switch (env.toLowerCase()) {
+  case "local":
+    module.exports = require("./config.dev");
+    return;
   case "dev":
   case "development":
-    module.exports = require("./config.dev");
+    module.exports = require("./config.prod.dev");
     return;
   case "prod":
   case "production":
   default:
-    module.exports = require("./config.prod");
+    module.exports = require("./config.prod.prod");
     return;
 }
